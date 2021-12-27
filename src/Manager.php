9e50d3176cdacc050927fbddd35a43c21a297566
Yunus Emre Deligöz <ye@deligoz.me>
2021-12-27 23:14:36 +0300

    Manager: It removes the cached generators file on uninstall

--- src/Manager.php
+++ src/Manager.php
@@ -4,6 +4,42 @@ declare(strict_types=1);
 
 namespace Phonyland\GeneratorManager;
 
-class Manager {
+use Composer\Composer;
+use Composer\IO\IOInterface;
+use Composer\Plugin\PluginInterface;
 
+class Manager implements PluginInterface
+{
+    public const GENERATOR_CACHE_FILE = 'phonyland-generators.json';
+
+    private Composer $composer;
+    private IOInterface $io;
+
+    /**
+     * {@inheritdoc}
+     */
+    public function activate(Composer $composer, IOInterface $io): void
+    {
+        $this->composer = $composer;
+        $this->io       = $io;
+    }
+
+    /** {@inheritdoc} */
+    public function deactivate(Composer $composer, IOInterface $io): void
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function uninstall(Composer $composer, IOInterface $io): void
+    {
+        $vendorDirectory = $composer->getConfig()->get('vendor-dir');
+        $pluginFile      = sprintf('%s/%s', $vendorDirectory, self::GENERATOR_CACHE_FILE);
+
+        if (file_exists($pluginFile)) {
+            unlink($pluginFile);
+        }
+    }
 }
--- tests/ManagerTest.php
+++ tests/ManagerTest.php
@@ -1,5 +1,21 @@
 <?php
 
+use Composer\Factory;
+use Composer\IO\NullIO;
 use Phonyland\GeneratorManager\Manager;
 
+beforeEach(function () {
+    $this->manager = new Manager();
+    $this->io = new NullIO();
+    $this->composer = (new Factory())->createComposer($this->io);
+});
+
 it('exists')->assertTrue(class_exists(Manager::class));
+
+it('removes the cached generators file on uninstall', function () {
+    touch('vendor/phonyland-generators.json');
+
+    $this->manager->uninstall($this->composer, $this->io);
+
+    $this->assertFileDoesNotExist('vendor/phonyland-generators.json');
+});
