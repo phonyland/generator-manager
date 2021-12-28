26bfc4609b5b523f429f3e9fec678be37ee5ae51
Yunus Emre Deligöz <ye@deligoz.me>
2021-12-28 23:46:16 +0300

    DumpCommand: Execution logic

--- src/Commands/DumpCommand.php
+++ src/Commands/DumpCommand.php
@@ -3,6 +3,13 @@
 declare(strict_types=1);
 
 namespace Phonyland\GeneratorManager\Commands;
+
+use Composer\Command\BaseCommand;
+use Phonyland\GeneratorManager\Manager;
+use RuntimeException;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Output\OutputInterface;
+
 /**
  * @internal
  */
@@ -11,6 +18,42 @@ final class DumpCommand extends BaseCommand
     protected function configure(): void
     {
         $this->setName('phonyland:dump-generators');
+    }
+
+    /**
+     * @throws \JsonException
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $composer = $this->getComposer();
+
+        if ($composer === null) {
+            throw new RuntimeException('Could not get Composer\Composer instance.');
+        }
+
+        $vendorDirectory = $composer->getConfig()->get('vendor-dir');
+        $generators = [];
+
+        $packages = $composer->getRepositoryManager()->getLocalRepository()->getCanonicalPackages();
+        $packages[] = $composer->getPackage();
+
+        /** @var \Composer\Package\PackageInterface $package */
+        foreach ($packages as $package) {
+            if (!isset($package->getExtra()['phonyland']['generators'])) {
+                continue;
+            }
+
+            $generators[] = $package->getExtra()['phonyland']['generators'];
+        }
+
+        $generators = array_merge(...$generators);
+
+        file_put_contents(
+            filename: implode(DIRECTORY_SEPARATOR, [$vendorDirectory, Manager::GENERATOR_CACHE_FILE]),
+            data: json_encode($generators, JSON_THROW_ON_ERROR | JSON_PRETTY_PRINT),
+        );
+
+        return 0;
     }
 
 }
