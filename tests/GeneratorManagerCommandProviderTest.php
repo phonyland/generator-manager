9e05642a12ee44870eed57cfb4af5e968fedd102
Yunus Emre Deligöz <ye@deligoz.me>
2021-12-29 00:02:37 +0300

    GeneratorManagerCommandProviderTest: It returns the dump command

--- tests/GeneratorManagerCommandProviderTest.php
+++ tests/GeneratorManagerCommandProviderTest.php
@@ -2,6 +2,15 @@
 
 declare(strict_types=1);
 
+use Phonyland\GeneratorManager\Commands\DumpCommand;
 use Phonyland\GeneratorManager\GeneratorManagerCommandProvider;
 
 it('exists')->assertTrue(class_exists(GeneratorManagerCommandProvider::class));
+
+it('returns the dump command', function () {
+    $commandProvider = new GeneratorManagerCommandProvider();
+    $commands = $commandProvider->getCommands();
+
+    $this->assertCount(1, $commands);
+    $this->assertInstanceOf(DumpCommand::class, $commands[0]);
+});
