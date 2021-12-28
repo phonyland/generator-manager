5e00eedcdfffcddbbdce85d7164d7475c0524a3c
Yunus Emre Deligöz <ye@deligoz.me>
2021-12-29 00:01:52 +0300

    GeneratorManagerCommandProviderTest: It exists

---
+++ tests/GeneratorManagerCommandProviderTest.php
@@ -0,0 +1,7 @@
+<?php
+
+declare(strict_types=1);
+
+use Phonyland\GeneratorManager\GeneratorManagerCommandProvider;
+
+it('exists')->assertTrue(class_exists(GeneratorManagerCommandProvider::class));
