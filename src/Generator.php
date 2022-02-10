ffbb10f966ccfa5f3f01550b415237260bfbc688
Yunus Emre Deligöz <ye@deligoz.me>
2022-02-11 02:13:44 +0300

    Generator: Require a Phony instance reference from the constructor

--- src/Generator.php
+++ src/Generator.php
@@ -4,6 +4,12 @@ declare(strict_types=1);
 
 namespace Phonyland\GeneratorManager;
 
+use Phonyland\Framework\Phony;
+
 abstract class Generator
 {
+    public function __construct(
+        protected Phony $phony
+    ) {
+    }
 }
